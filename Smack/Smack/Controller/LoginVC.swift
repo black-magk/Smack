d5d79ff7b124604a546317e6da7b67642c1fe4da
Landon Carr <landoncarr@Landons-MacBook-Pro.local>
2018-03-26 16:56:32 -0600

    Back from Gym, UI near Done

--- Smack/Smack/Controller/LoginVC.swift
+++ Smack/Smack/Controller/LoginVC.swift
@@ -1,8 +1,9 @@
 //
 //  LoginVC.swift
-//  
+//  Smack
 //
 //  Created by Landon Carr on 3/26/18.
+//  Copyright © 2018 Landon Carr. All rights reserved.
 //
 
 import UIKit
@@ -15,9 +16,10 @@ class LoginVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    @IBAction func xBtnPressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
     }
     
 
